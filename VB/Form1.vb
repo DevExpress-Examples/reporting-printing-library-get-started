fed2eff96143fa32fa14cdf08de68e57851c7069
DevExpressExampleBot <fakeEmail>
2022-11-24 01:22:50 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,82 +1,70 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports System.Drawing
 Imports DevExpress.XtraPrinting
-' ...
 
+' ...
 Namespace docGettingStarted
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
+
         Public Sub New()
             InitializeComponent()
         End Sub
 
         Private imagePath As String = "..\..\fish.bmp"
 
-        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button1.Click
-            Dim lesson As New Lesson1(printingSystem1)
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim lesson As Lesson1 = New Lesson1(printingSystem1)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button2.Click
-            Dim lesson As New Lesson2(printingSystem1)
+        Private Sub button2_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim lesson As Lesson2 = New Lesson2(printingSystem1)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button3_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button3.Click
-            Dim lesson As New Lesson3(printingSystem1)
+        Private Sub button3_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim lesson As Lesson3 = New Lesson3(printingSystem1)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button4_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button4.Click
-            Dim lesson As New Lesson4(printingSystem1)
+        Private Sub button4_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim lesson As Lesson4 = New Lesson4(printingSystem1)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button5_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button5.Click
-            Dim lesson As New Lesson5(printingSystem1)
+        Private Sub button5_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim lesson As Lesson5 = New Lesson5(printingSystem1)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button6_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button6.Click
-            Dim lesson As New Lesson6(printingSystem1)
+        Private Sub button6_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim lesson As Lesson6 = New Lesson6(printingSystem1)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button7_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button7.Click
+        Private Sub button7_Click(ByVal sender As Object, ByVal e As EventArgs)
             Dim img As Bitmap = CType(Bitmap.FromFile(imagePath), Bitmap)
             img.MakeTransparent()
-
-            Dim lesson As New Lesson7(printingSystem1, img)
+            Dim lesson As Lesson7 = New Lesson7(printingSystem1, img)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button8_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button8.Click
+        Private Sub button8_Click(ByVal sender As Object, ByVal e As EventArgs)
             Dim img As Bitmap = CType(Bitmap.FromFile(imagePath), Bitmap)
             img.MakeTransparent()
-
-            Dim lesson As New Lesson8(printingSystem1, img)
+            Dim lesson As Lesson8 = New Lesson8(printingSystem1, img)
             lesson.ShowPreview()
         End Sub
 
-        Private Sub button9_Click(ByVal sender As Object, ByVal e As EventArgs) _
-        Handles button9.Click
+        Private Sub button9_Click(ByVal sender As Object, ByVal e As EventArgs)
             Dim img As Bitmap = CType(Bitmap.FromFile(imagePath), Bitmap)
             img.MakeTransparent()
-
-            Dim lesson As New Lesson9(printingSystem1, img)
+            Dim lesson As Lesson9 = New Lesson9(printingSystem1, img)
             lesson.ShowPreview()
         End Sub
-
     End Class
 
     Public Class Lesson1
@@ -87,31 +75,29 @@ Namespace docGettingStarted
         End Sub
     End Class
 
-
     Public Class Lesson2
         Inherits Lesson1
 
         Friend top As Integer = 0
 
+        Friend r As Rectangle = New Rectangle(0, 0, 150, 50)
+
+        Friend caption As String = "Hello World!"
+
         Public Sub New(ByVal ps As PrintingSystem)
             MyBase.New(ps)
         End Sub
 
-        Protected Overloads Overrides Sub BeforeCreate()
+        Protected Overrides Sub BeforeCreate()
             MyBase.BeforeCreate()
-
-            If Not Me.PrintingSystem Is Nothing Then
-                Dim g As BrickGraphics = Me.PrintingSystem.Graph
-
+            If PrintingSystem IsNot Nothing Then
+                Dim g As BrickGraphics = PrintingSystem.Graph
                 ' Set the background color to White.
                 g.BackColor = Color.White
-
                 ' Set the border color to Black.
                 g.BorderColor = Color.Black
-
                 ' Set the font to the default font.
                 g.Font = g.DefaultFont
-
                 ' Set the line alignment.
                 g.StringFormat = g.StringFormat.ChangeLineAlignment(StringAlignment.Near)
             End If
@@ -119,12 +105,9 @@ Namespace docGettingStarted
 
         ' Add a text brick without borders with a "Hello World!" text.
         Protected Overrides Sub CreateDetail(ByVal graph As BrickGraphics)
-            Dim r As RectangleF = New RectangleF(0, 0, 150, 50)
-            Dim s As String = "Hello World!"
-            graph.DrawString(s, Color.Black, r, BorderSide.None)
+            graph.DrawString(caption, Color.Black, r, BorderSide.None)
         End Sub
     End Class
-
 
     Public Class Lesson3
         Inherits Lesson2
@@ -136,20 +119,16 @@ Namespace docGettingStarted
         ' Set the background color to Deep Sky Blue.
         Protected Overrides Sub CreateDetail(ByVal graph As BrickGraphics)
             graph.BackColor = Color.DeepSkyBlue
-
             ' Set the border color to Midnight Blue.
             graph.BorderColor = Color.MidnightBlue
-
             ' Add a text brick with all borders and a "Hello World!" text.
-            Dim r As RectangleF = New RectangleF(0, 0, 150, 50)
-            Dim s As String = "Hello World!"
-            graph.DrawString(s, Color.Red, r, BorderSide.All)
+            graph.DrawString(caption, Color.Red, r, BorderSide.All)
         End Sub
     End Class
 
-
     Public Class Lesson4
         Inherits Lesson3
+
         Public Sub New(ByVal ps As PrintingSystem)
             MyBase.New(ps)
         End Sub
@@ -161,9 +140,9 @@ Namespace docGettingStarted
         End Sub
     End Class
 
-
     Public Class Lesson5
         Inherits Lesson4
+
         Public Sub New(ByVal ps As PrintingSystem)
             MyBase.New(ps)
         End Sub
@@ -171,7 +150,6 @@ Namespace docGettingStarted
         Protected Overrides Sub CreateDetail(ByVal graph As BrickGraphics)
             ' Center the text string.
             graph.StringFormat = graph.StringFormat.ChangeLineAlignment(StringAlignment.Center)
-
             MyBase.CreateDetail(graph)
             CreateRow(graph)
         End Sub
@@ -179,19 +157,24 @@ Namespace docGettingStarted
         Protected Overridable Sub CreateRow(ByVal graph As BrickGraphics)
             ' Set the brick font name to Arial, size to 14, and set the bold attribute.
             graph.Font = New Font("Arial", 14, FontStyle.Bold)
-
-            top += 50
-
             ' Add a text brick with all borders to a specific location 
             ' with a "Good-bye!" text using the blue font color.
-            graph.DrawString("Good-bye!", Color.Blue, _
-                New RectangleF(0, top, 150, 50), BorderSide.All)
+            graph.DrawString("Good-bye!", Color.Blue, New Rectangle(0, CSharpImpl.__Assign(top, 50), 150, 50), BorderSide.All)
         End Sub
-    End Class
 
+        Private Class CSharpImpl
+
+            <Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
+            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
+                target = value
+                Return value
+            End Function
+        End Class
+    End Class
 
     Public Class Lesson6
         Inherits Lesson5
+
         Public Sub New(ByVal ps As PrintingSystem)
             MyBase.New(ps)
         End Sub
@@ -199,35 +182,28 @@ Namespace docGettingStarted
         Protected Overrides Sub CreateDetail(ByVal graph As BrickGraphics)
             ' Center the text string.
             graph.StringFormat = graph.StringFormat.ChangeLineAlignment(StringAlignment.Center)
-
             ' Add an unchecked check box brick with all borders 
             ' to a specific location using the Light Sky Blue background color.
-            graph.DrawCheckBox(New RectangleF(150, 0, 50, 50), _
-                BorderSide.All, Color.LightSkyBlue, False)
-
+            graph.DrawCheckBox(New Rectangle(150, 0, 50, 50), BorderSide.All, Color.LightSkyBlue, False)
             ' Add an empty rectangle with all borders 
             ' to a specific location using the Light Lavender background color.
-            graph.DrawRect(New RectangleF(200, 0, 50, 50), _
-                BorderSide.All, Color.Lavender, graph.BorderColor)
-
+            graph.DrawRect(New Rectangle(200, 0, 50, 50), BorderSide.All, Color.Lavender, graph.BorderColor)
             MyBase.CreateDetail(graph)
         End Sub
 
         Protected Overrides Sub CreateRow(ByVal graph As BrickGraphics)
             MyBase.CreateRow(graph)
-
             ' Add a checked check box brick with all borders 
             ' to a specific location using the Light Sky Blue background color.
-            graph.DrawCheckBox(New RectangleF(150, top, 50, 50), _
-                BorderSide.All, Color.LightSkyBlue, True)
-
+            graph.DrawCheckBox(New Rectangle(150, top, 50, 50), BorderSide.All, Color.LightSkyBlue, True)
         End Sub
     End Class
 
-
     Public Class Lesson7
         Inherits Lesson6
+
         Private img As Bitmap
+
         Friend bkImageColor As Color = Color.Lavender
 
         Public Sub New(ByVal ps As PrintingSystem, ByVal img As Bitmap)
@@ -238,26 +214,18 @@ Namespace docGettingStarted
 
         Protected Overrides Sub CreateRow(ByVal graph As BrickGraphics)
             MyBase.CreateRow(graph)
-
             ' Add an empty rectangle with all borders 
             ' to a specific location using a Lavender background color.
-            graph.DrawRect(New RectangleF(200, top, 50, 50), _
-                BorderSide.All, bkImageColor, graph.BorderColor)
-
+            graph.DrawRect(New Rectangle(200, top, 50, 50), BorderSide.All, bkImageColor, graph.BorderColor)
             ' Add an image without borders 
             ' to a specific location using a Transparent color.
-            If Not img Is Nothing Then
-                graph.DrawImage(img, New RectangleF(Convert.ToSingle(200 + (50 - img.Width) / 2), _
-                    Convert.ToSingle(top + (50 - img.Height) / 2), img.Width, img.Height), _
-                    BorderSide.None, Color.Transparent)
-            End If
+            If img IsNot Nothing Then graph.DrawImage(img, New Rectangle(200 + (50 - img.Width) \ 2, top + (50 - img.Height) \ 2, img.Width, img.Height), BorderSide.None, Color.Transparent)
         End Sub
-
     End Class
-
 
     Public Class Lesson8
         Inherits Lesson7
+
         Public Sub New(ByVal ps As PrintingSystem, ByVal img As Bitmap)
             MyBase.New(ps, img)
         End Sub
@@ -265,31 +233,23 @@ Namespace docGettingStarted
         Protected Overrides Sub CreateDetailHeader(ByVal graph As BrickGraphics)
             ' Center a text string horizontally and vertically.
             graph.StringFormat = New BrickStringFormat(StringAlignment.Center, StringAlignment.Center)
-
             ' Set the brick font name to Comic Sans MS, size to 12.
             graph.Font = New Font("Comic Sans MS", 12)
-
             ' Set the background color to Light Green.
             graph.BackColor = Color.LightGreen
-
             ' Add a text brick with all borders to a specific location 
             ' with an "I" text string using a Green font color.
-            graph.DrawString("I", Color.Green, New RectangleF(0, 0, 150, 25), BorderSide.All)
-
+            graph.DrawString("I", Color.Green, New Rectangle(0, 0, 150, 25), BorderSide.All)
             ' Add a text brick with all borders to a specific location 
             ' with a "love" text string using a Green font color.
-            graph.DrawString("love", Color.Green, New RectangleF(150, 0, 50, 25), BorderSide.All)
-
+            graph.DrawString("love", Color.Green, New Rectangle(150, 0, 50, 25), BorderSide.All)
             ' Add a text brick with all borders to a specific location 
             ' with a "you" text string using a Green font color.
-            graph.DrawString("you", Color.Green, New RectangleF(200, 0, 50, 25), BorderSide.All)
-
+            graph.DrawString("you", Color.Green, New Rectangle(200, 0, 50, 25), BorderSide.All)
             ' Set the line alignment.
             graph.StringFormat = graph.StringFormat.ChangeAlignment(StringAlignment.Near)
-
         End Sub
     End Class
-
 
     Public Class Lesson9
         Inherits Lesson8
@@ -301,59 +261,37 @@ Namespace docGettingStarted
         Protected Overrides Sub CreateRow(ByVal graph As BrickGraphics)
             ' Set the number of iterations for row creation.
             Dim c As Integer = 230
-
-            Dim i As Integer
-            For i = 0 To 49
-
+            For i As Integer = 0 To 50 - 1
                 ' Set the background color using RGB.
                 bkImageColor = Color.FromArgb(c, c, c + 20)
-
                 MyBase.CreateRow(graph)
-                If c - 4 > 0 Then
-                    c = c - 4
-                Else
-                    c = c
-                End If
+                c = If(c - 4 > 0, c - 4, c)
             Next
         End Sub
 
         Protected Overrides Sub CreateMarginalHeader(ByVal graph As BrickGraphics)
             ' Set the format string for a page info brick.
             Dim format As String = "Page {0} of {1}"
-
             ' Set font to the default font.
             graph.Font = graph.DefaultFont
-
             ' Set the background color to Transparent.
             graph.BackColor = Color.Transparent
-
             ' Set the rectangle for drawing.
             Dim r As RectangleF = New RectangleF(0, 0, 0, graph.Font.Height)
-
             ' Add a page info brick without borders that displays
             ' the current page number from the total number of pages.
-            Dim brick As PageInfoBrick = graph.DrawPageInfo(PageInfo.NumberOfTotal, _
-                format, Color.Black, r, BorderSide.None)
-
+            Dim brick As PageInfoBrick = graph.DrawPageInfo(PageInfo.NumberOfTotal, format, Color.Black, r, BorderSide.None)
             ' Set brick alignment.
             brick.Alignment = BrickAlignment.Far
-
             ' Enable auto width for a brick.
             brick.AutoWidth = True
-
             ' Add a page info brick without borders 
             ' that displays date and time.
-            brick = graph.DrawPageInfo(PageInfo.DateTime, "", Color.Black, r, _
-                BorderSide.None)
-
+            brick = graph.DrawPageInfo(PageInfo.DateTime, "", Color.Black, r, BorderSide.None)
             ' Set brick alignment.
             brick.Alignment = BrickAlignment.Near
-
             ' Enable auto width for a brick.
             brick.AutoWidth = True
         End Sub
-
     End Class
-
-
 End Namespace
--- VB/Properties/AssemblyInfo.vb
+++ VB/Properties/AssemblyInfo.vb
@@ -1,28 +1,23 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Reflection
-Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("GettingStartedCS")>
-<Assembly: AssemblyDescription("")>
-<Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("-")>
-<Assembly: AssemblyProduct("GettingStartedCS")>
-<Assembly: AssemblyCopyright("Copyright © - 2007")>
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
+<Assembly:AssemblyTitle("GettingStartedCS")>
+<Assembly:AssemblyDescription("")>
+<Assembly:AssemblyConfiguration("")>
+<Assembly:AssemblyCompany("-")>
+<Assembly:AssemblyProduct("GettingStartedCS")>
+<Assembly:AssemblyCopyright("Copyright © - 2007")>
+<Assembly:AssemblyTrademark("")>
+<Assembly:AssemblyCulture("")>
 ' Setting ComVisible to false makes the types in this assembly not visible 
 ' to COM components.  If you need to access a type in this assembly from 
 ' COM, set the ComVisible attribute to true on that type.
-<Assembly: ComVisible(False)>
-
+<Assembly:ComVisible(False)>
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("d2728ca8-7d7e-42fa-b924-74afd29f0789")>
-
+<Assembly:Guid("d2728ca8-7d7e-42fa-b924-74afd29f0789")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -30,5 +25,5 @@ Imports System.Runtime.InteropServices
 '      Build Number
 '      Revision
 '
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly:AssemblyVersion("1.0.0.0")>
+<Assembly:AssemblyFileVersion("1.0.0.0")>
